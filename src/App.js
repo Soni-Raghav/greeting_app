841f841e0355c21a6d4ddcef0fc79a8c2bd6bc07
Soni-Raghav <sonii.raghav@gmail.com>
2023-10-13 17:52:16 +0530

    greeting app created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const date = new Date();
+const hh = date.getHours();
+// const mm=date.getMinutes();
+// const ss=date.getSeconds();
+// const time = date.toLocaleTimeString();//
+let message = "";
+const cssStyle={};
 
+
+  if (hh >= 0 && hh <= 12) {
+    message = "Good Morning!..";
+    cssStyle.color="green"
+  } else if (hh > 12 && hh <= 16) {
+    message = "Good Afternoon!..";
+    cssStyle.color="Red"
+    
+  } else if (hh > 16 && hh <= 19) {
+    message = "Good Evening!..";
+    cssStyle.color="blue"
+    
+  } else if (hh > 19 && hh <= 23) {
+    message = "Good Night!..";
+    cssStyle.color="white"
+   
+  }
+  
+
+// if
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="greetingBox">
+          <h1>Hello Sir, <span style={cssStyle}>{message}</span></h1>
+        </div>
+      </div>
+    </>
   );
 }
 
